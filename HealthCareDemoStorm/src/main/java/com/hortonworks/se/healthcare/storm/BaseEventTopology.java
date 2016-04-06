594f155e6fdc3ee19836c0a477a97d4674c44715
zeltovhorton <azeltov@hortonworks.com>
2016-04-05 20:59:30 -0700

    debug

--- HealthCareDemoStorm/src/main/java/com/hortonworks/se/healthcare/storm/BaseEventTopology.java
+++ HealthCareDemoStorm/src/main/java/com/hortonworks/se/healthcare/storm/BaseEventTopology.java
@@ -18,6 +18,8 @@ public abstract class BaseEventTopology {
 		
 		topologyConfig = new Properties();
 		try {
+	    	System.out.println("configfile=" + configFileLocation);
+
 			topologyConfig.load(ClassLoader.getSystemResourceAsStream(configFileLocation));
 		} catch (FileNotFoundException e) {
 			LOG.error("Encountered error while reading configuration properties: "
