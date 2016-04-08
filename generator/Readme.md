552475901c2b8200db15ba0062c438f8a7d664fb
vzlatkin <vzlatkin@gmail.com>
2016-04-08 10:10:34 -0400

    change to generate one file per metric with all patients inside

--- generator/Readme.md
+++ generator/Readme.md
@@ -1,10 +1,5 @@
 Copy all files into the same directory, install Java 8 and then execute
 
-<pre>
-/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.77-0.b03.el6_7.x86_64/jre/bin/java  -Dlogging.config=logback.xml  -jar data-generator-1.1.0.jar
-</pre>
-
-#### Artem notes
 ##### install Java 8
 ```
 sh jdk8.sh
--- generator/generate.sh
+++ generator/generate.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-rm -f *.json *.txt
+mkdir -p /tmp/vitals
+chmod goa+x /tmp/vitals/
+chmod a+w -R /tmp/vitals
+rm -f *.json /tmp/vitals/*.txt
 
 names=$(cat patients.csv  | tail -n +2 | cut -d, -f3)
 for n in $names; do
 
-    start=$(date -d "-1 days" +"%Y-%m-%dT%H:%M:%SZ")
-    end=$(date -d "+1 days" +"%Y-%m-%dT%H:%M:%SZ")
+    start=$(date -d "-1 hour" +"%Y-%m-%dT%H:%M:%SZ")
+    end=$(date -d "+1 hour" +"%Y-%m-%dT%H:%M:%SZ")
 
     perl -pe "s/(start-date-time: ).*/start-date-time: $start/g" -i application.yaml
     perl -pe "s/(end-date-time: ).*/end-date-time: $end/g" -i application.yaml
@@ -16,5 +19,5 @@ for n in $names; do
 done
 
 for m in blood-pressure respiratory-rate heart-rate oxygen-saturation; do
-    grep $m *.json > $m.json
+    grep $m *.json > /tmp/vitals/$m.json
 done
