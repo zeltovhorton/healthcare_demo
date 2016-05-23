157426ef0bd03c7348dcaf77581c2069fa99f622
vzlatkin <vzlatkin@gmail.com>
2016-05-23 12:53:40 -0400

    added a new NiFi tempate that is simpler, but not yet working

--- generator/generate.sh
+++ generator/generate.sh
@@ -10,14 +10,14 @@ names=$(cat patients.csv  | tail -n +2 | cut -d, -f3)
 for n in $names; do
 
     start=$(date -d "-10 minutes" +"%Y-%m-%dT%H:%M:%SZ")
-    end=$(date -d "+10 minutes" +"%Y-%m-%dT%H:%M:%SZ")
+    end=$(date -d "now"  +"%Y-%m-%dT%H:%M:%SZ")
 
     perl -pe "s/(start-date-time: ).*/start-date-time: $start/g" -i application.yaml
     perl -pe "s/(end-date-time: ).*/end-date-time: $end/g" -i application.yaml
 
     perl -pe "s/filename: .*/filename: \.\/generated\/$n.json/g" -i application.yaml
     perl -pe "s/user-id: .*/user-id: $n/g" -i application.yaml
-    java -Dlogging.config=logback.xml  -jar data-generator*.jar
+    /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java -Dlogging.config=logback.xml  -jar data-generator*.jar
 done
 
 for m in blood-pressure respiratory-rate heart-rate oxygen-saturation; do
